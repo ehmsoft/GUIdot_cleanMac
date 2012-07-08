f9aa3878f2623907eaba23d91301ccdbbd8e2380
Andrés Torres <elfotografo007@gmail.com>
2012-07-08 10:49:49 -0500

    Initial Commit

---
+++ GUIdot_cleanMac/com/ehmsoft/dotclean/GUIdot_cleanMac.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 08/07/2012
+
+@author: elfotografo007
+'''
+import os
+import sys
+import shutil
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+app = QApplication(sys.argv)
+
+sys.exit(app.exec_())
